395a0abad913af16c397f34a7991fcd0754effb4
VDamnV <dmytriivolkov@gmail.com>
2026-04-30 16:04:09 +0300

    Update MappingProfile.cs

--- Lab_3.BLL/Mapping/MappingProfile.cs
+++ Lab_3.BLL/Mapping/MappingProfile.cs
@@ -9,14 +9,13 @@ namespace Lab_3.BLL.Mapping
     {
         public MappingProfile()
         {
-            // Маппінг: Сутність бази даних -> DTO (передача на UI)
+            // Сутність бази даних -> DTO
             CreateMap<Room, RoomDto>()
-                // Явно вказуємо, що enum RoomStatus треба перетворити в рядок
                 .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()));
 
             CreateMap<Booking, BookingDto>();
 
-            // Зворотний маппінг: DTO -> Сутність (якщо колись знадобиться зберігати DTO напряму)
+            // DTO -> Сутність
             CreateMap<RoomDto, Room>()
                 .ForMember(dest => dest.Status, opt => opt.MapFrom(src => Enum.Parse<RoomStatus>(src.Status)));
                 
